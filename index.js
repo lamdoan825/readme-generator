847f13cf7e13352b314fc62cca3aed752ce6325e
lamdoan825@gmail.com <lam@Lams-MacBook-Air.local>
2021-05-19 18:22:13 -0500

    npm initialized and inquirer installed

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer')
+
+// array of questions for user
+const questions = [
+
+  {
+      type: "input",
+      name: "github",
+      message: "What's your Github username?"
+  }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+}
+// function call to initialize program
+init();
