eeabf214069279ef042f43c037926db13a972886
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-05-26 01:15:45 -0500

    created a partially working test

---
+++ dist/README.md
@@ -0,0 +1,55 @@
+# test
+
+
+## Description
+
+
+
+## Table of Contents
+
+* [Dependencies](#dependencies)
+
+* [Usage](#usage)
+
+
+* [License](#license)
+
+
+* [Contributors](#contributors)
+
+* [Test](#test)
+
+* [Questions](#questions)
+
+## Dependencies
+
+To install dependencies, run these commands:
+
+```
+npm i
+```
+
+## Usage
+
+
+
+## License
+
+This project is licensed by undefined.
+
+## Contributors
+
+
+test
+[test](https://github.com/)
+
+## Tests
+
+To run tests, run these commands:
+
+```
+npm test
+node index.js
+```
+
+
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const inquirer = require('inquirer')
-// const generatePage = require('');
-// const { writeFile, copyFile } = require('');
+const inquirer = require('inquirer');
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // array of questions for user
 const questions = [
@@ -23,12 +25,21 @@ const questions = [
 
 ];
 
+
 // function to write README file
 function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+  inquirer.prompt(questions)
+      .then((inquirerAnswers) => {
+          console.log("Generating....");
+          writeToFile("README.md", generateMarkdown({ ...inquirerAnswers }));
+      })
+
 }
+
 // function call to initialize program
 init();
